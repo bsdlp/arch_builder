102e369b25e260a50e93b7a0ab14f534631554f1
Jon Chen <bsd@voltaire.sh>
2014-06-29 00:55:56 -0400

    add repo configurator

--- arch_builder.py
+++ arch_builder.py
@@ -3,14 +3,26 @@
 from github3 import login
 from getpass import getpass
 
+
 username = ''
 password = ''
+repo = []
+
+try:
+    import config
+    username = config.username
+    repo = config.repo.split('/', maxsplit=1)
+except ImportError:
+    pass
 
 while not username:
     username = input('GitHub Username: ')
 
 while not password:
     password = getpass('Password for {0}: '.format(username))
+
+while not repo:
+    repo = input('GitHub Repo (i.e. fly/arch): ').split('/', maxsplit=1)
 
 def _two_fa_input():
     two_fa = ''
@@ -19,3 +31,5 @@ def _two_fa_input():
     return two_fa
 
 g = login(username=username, password=password, two_factor_callback=_two_fa_input)
+
+repository = g.repository(repo[0], repo[1])
